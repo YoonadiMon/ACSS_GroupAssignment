82ec296b642cf24168fc2f819aa4212e3d835060
hp <hp@10.101.138.212>
2025-03-17 15:54:58 +0800

    Emma changed smth

--- ACSS/src/MainProgram/MainProgram.java
+++ ACSS/src/MainProgram/MainProgram.java
@@ -6,6 +6,7 @@ public class MainProgram {
     public static void main(String[] args) {
         // This is for main program
         System.out.println("Testing");
+        System.out.println("Ready for programming?");
     }
     
 }
